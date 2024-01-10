3009996a24883716a8a05da8dff18d2410c42863
Vladimir Lima <vladimirflima@outlook.com>
2024-01-10 11:45:47 -0300

    9o
    remoção de algumas coisa dentro do código

--- script-cadastro.js
+++ script-cadastro.js
@@ -13,14 +13,11 @@ document.addEventListener('DOMContentLoaded', function () {
         const username = document.getElementById('signup-username').value;
         const password = document.getElementById('signup-password').value;
 
-        // Adicione lógica para enviar os dados do cadastro para o servidor ou armazenar localmente
-
-        // Exemplo de armazenamento local (não seguro para produção)
         localStorage.setItem('username', username);
         localStorage.setItem('password', password);
 
         alert('Cadastro realizado com sucesso! Faça login agora.');
-        // Redirecione para a página de login ou outra página desejada
+ 
         window.location.href = 'login.html';
     });
 });
